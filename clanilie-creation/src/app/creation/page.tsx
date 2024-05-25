797b2eb0218505efcbb12d06f9d5c259706cc319
raphael-attias <raphael.attias@laplateforme.io>
2024-05-26 00:33:12 +0200

    premier pas

---
+++ clanilie-creation/src/app/creation/page.tsx
@@ -0,0 +1,6 @@
+import { Navbar } from 
+
+const creation = () => {
+
+    
+}
---
+++ clanilie-creation/src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+import  Navbar  from "./compoments/Navbar";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <Navbar />
+    </div>
+    </>
+  );
+}
