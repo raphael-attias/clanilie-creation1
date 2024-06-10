82634e880f28deace316a11252f7dc846b0944e8
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2024-06-10 13:55:40 +0200

    restructuration du site

---
+++ site-next-js/app/compoments/Footer.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Footer
--- site-next-js/app/contact/page.tsx
+++ site-next-js/app/contact/page.tsx
@@ -25,25 +25,25 @@ const Contact = () => {
     <main className="bg-slate-50">
       <div>
         <Navbar />
-        <h1 className="text-center text-xl text-[#ff6dc6] m-5">Contactez-nous !</h1>
+        <h1 className="text-center text-xl text-[#016435] m-5">Contactez-nous !</h1>
         <form onSubmit={submitForm} className="max-w-xl mx-auto bg-white rounded-lg p-8 shadow-md m-3">
-          <label className="block mb-2 text-[#ff6dc6]">
+          <label className="block mb-2 text-[#016435]">
             Nom :
             <input type="text" name="name" onChange={handleChange} className="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring-[#ff6dc6] focus:border-[#ff6dc6]" />
           </label>
-          <label className="block mb-2 text-[#ff6dc6]">
+          <label className="block mb-2 text-[#016435]">
             Prénom :
             <input type="text" name="surname" onChange={handleChange} className="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring-[#ff6dc6] focus:border-[#ff6dc6]" />
           </label>
-          <label className="block mb-2 text-[#ff6dc6]">
+          <label className="block mb-2 text-[#016435]">
             Email :
             <input type="email" name="email" onChange={handleChange} className="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring-[#ff6dc6] focus:border-[#ff6dc6]" />
           </label>
-          <label className="block mb-2 text-[#ff6dc6]">
+          <label className="block mb-2 text-[#016435]">
             Message :
             <textarea name="message" onChange={handleChange} className="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring-[#ff6dc6] focus:border-[#ff6dc6]"></textarea>
           </label>
-          <input type="submit" value="Envoyer" className="bg-[#ff6dc6] text-white rounded-md px-6 py-2 hover:bg-[#ff6dc6] cursor-pointer" />
+          <input type="submit" value="Envoyer" className="bg-[#016435] text-white rounded-md px-6 py-2 hover:bg-[#ff6dc6] cursor-pointer" />
         </form>
       </div>
     </main>
